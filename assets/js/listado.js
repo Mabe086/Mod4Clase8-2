17bf92e371e1d5ef82e3152efa559edded988bf1
Mabel Saavedra <mabel.saavedra03@inacapmail.cl>
2023-05-18 19:24:27 -0400

    base del ejercicio clase anterior

---
+++ assets/js/listado.js
@@ -0,0 +1,13 @@
+// const apiKey = '548794004117088';
+
+// // Realiza una solicitud a la API utilizando la clave de API
+// fetch(`https://superheroapi.com/api/${apiKey}/endpoint`)
+//   .then(response => response.json())
+//   .then(data => {
+//     // Aquí puedes utilizar los datos de la respuesta de la API
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     // Manejo de errores
+//     console.error(error);
+//   });
